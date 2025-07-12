ce85dc9c6282b74614dd7fbc4336f8fb7140f7a1
AlexisCelis14 <ar.alexiscelis14@gmail.com>
2025-07-11 21:51:19 -0600

    Commit inicial, creacion del proyecto

---
+++ src/test/java/com/mastertyres/MasterTyresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastertyres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterTyresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
