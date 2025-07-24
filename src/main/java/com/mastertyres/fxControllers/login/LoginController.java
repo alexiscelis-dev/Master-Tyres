a9481709ebf8f70f95d76db4c289494cae0396a9
Alexis Celis <celis.alexis.1hm@gmail.com>
2025-07-23 23:33:51 -0600

    Se agregaron vistas clientes, vehiculos, Controlladores, modificacion Login Controller

--- src/main/java/com/mastertyres/fxControllers/login/LoginController.java
+++ src/main/java/com/mastertyres/fxControllers/login/LoginController.java
@@ -1,8 +1,11 @@
 package com.mastertyres.fxControllers.login;
 
+import com.mastertyres.fxControllers.ventanaPrincipal.VentanaPrincipalController;
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
+import javafx.stage.Screen;
 import org.springframework.stereotype.Component;
 
 import javafx.event.ActionEvent;
@@ -21,23 +24,51 @@ public class LoginController {
 
 
     @FXML
-    private void cambiarAVistaPrincipal(ActionEvent event) {
+    private void logIn(ActionEvent event){
+        cambiarVistaPrincipal(event, "/fxml_views/Master_Tyres_Principal.fxml");
+    }
+
+    private void cambiarVistaPrincipal(ActionEvent event,String archivoFXML){
+
+        try {
+            Parent root = FXMLLoader.load(VentanaPrincipalController.class.getResource(archivoFXML));
+            Stage ventanaLogin = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            ventanaLogin.setScene(new Scene(root));
+
+
+            Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+            ventanaLogin.setX(screenBounds.getMinX());
+            ventanaLogin.setY(screenBounds.getMinY());
+            ventanaLogin.setWidth(screenBounds.getWidth());
+            ventanaLogin.setHeight(screenBounds.getHeight());
+
+            ventanaLogin.show();
+
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+    }//cambiarVistaPrincipal
+
+   /* private void cambiarAVistaPrincipal(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml_views/Master_Tyres_Principal.fxml"));
             Parent root = loader.load();
 
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Stage ventanaPrincipal = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Scene scene = new Scene(root);
 
-            stage.setScene(scene);
-            stage.setMaximized(false);
-            stage.setMaximized(true);
-            stage.show();
+            ventanaPrincipal.setScene(scene);
+            ventanaPrincipal.setMaximized(false);
+            ventanaPrincipal.setMaximized(true);
+            ventanaPrincipal.show();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
 
 }
