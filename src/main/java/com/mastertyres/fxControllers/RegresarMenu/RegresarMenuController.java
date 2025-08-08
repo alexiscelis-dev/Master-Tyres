d497cdea2b71cfd30684223ad1e0add39f43a5db
Alexis Celis <celis.alexis.1hm@gmail.com>
2025-08-08 16:16:30 -0600

    pulling cambios Jayro, cambio color

--- src/main/java/com/mastertyres/fxControllers/RegresarMenu/RegresarMenuController.java
+++ src/main/java/com/mastertyres/fxControllers/RegresarMenu/RegresarMenuController.java
@@ -9,6 +9,7 @@ public class RegresarMenuController {
 /*Esta clase solamente es necesaria para hacer que se regrese al menu al momento de precionar a la imagen
 debido a que el table view y los componentes se agregan sobre el pane no se puede solo regresar se necesita mandar
 como parametro otro archivo FXML para cambiar de ventana (Una ventana vacia) es lo que es esta clase.
+
     */
 
     @FXML
